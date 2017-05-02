ba7c87931e51105482d3ed8afbd1aa4c7559ac6f
Zyta Wawrzeńczyk <zyta.wawrzenczyk@gmail.com>
2017-05-02 13:27:16 +0200

    render question

---
+++ js/question.js
@@ -0,0 +1,11 @@
+function buildAnswer(text, id) {
+  return `
+      <div id="answer-${id}" class="sg-box sg-box--full sg-media--clickable sg-box__container--auto-margin sg-text--centered" data-id="${id}">
+        ${text}
+      </div>
+  `;
+}
+
+function setQuestion(text) {
+  document.getElementById('question').innerHTML = text;
+}
--- js/quiz.js
+++ js/quiz.js
@@ -3,4 +3,17 @@ function showStartButton() {
 }
 function startQuiz() {
   document.getElementById('start-button-container').classList.add('sg-layout__container--hidden');
+  question = getNextQuestion();
+  setQuestion(question.question);
+  answersContainer = document.getElementById('answers');
+  answersContainer.innerHTML = '';
+  question.answers.forEach(answer => {
+    answersContainer.innerHTML += buildAnswer(answer.answer, answer.id);
+  });
+  document.getElementById('question-container').classList.remove('sg-layout__container--hidden');
+}
+function getNextQuestion() {
+  config.currentQuestion++;
+  return config.questions[config.currentQuestion];
+}
 }
