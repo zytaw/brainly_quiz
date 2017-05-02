2118be3206ae6a510718f776812eab9a587556bf
Zyta Wawrzeńczyk <zyta.wawrzenczyk@gmail.com>
2017-05-02 12:43:51 +0200

    fetch data

---
+++ index.js
@@ -0,0 +1,13 @@
+const myRequest = new Request('https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811ad96a0567648ff858b4f14d0096ba241f28ef/quiz-data.json');
+
+const config = {
+  questions: [],
+  currentQuestion: -1
+}
+
+fetch(myRequest)
+  .then(function(response) { return response.json(); })
+  .then(function(data) {
+    config.questions = data.questions;
+  }
+);
